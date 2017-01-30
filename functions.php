052b692f4825d7021586f4d79a2c5dd73faa3e25
Ryan Fitzgerald <rfitz042@uottawa.ca>
2017-01-30 09:42:19 -0500

    Fix names

--- functions.php
+++ functions.php
@@ -11,10 +11,10 @@
  *  Main Setup function for the theme. This uses the 'after_setup_theme' hook and therefore gets called
  *  once the page has loaded and the theme has been setup.
  */
-function citadelthemes_setup() {
+function origin_setup() {
 
     // Make theme available for translation
-    load_theme_textdomain( 'citadelthemes', get_template_directory() . '/languages' );
+    load_theme_textdomain( 'origin', get_template_directory() . '/languages' );
 
     // Add theme support for post thumbnails
     add_theme_support( 'post-thumbnails' );
@@ -31,37 +31,37 @@ function citadelthemes_setup() {
     ] );
 
 }
-add_action( 'after_setup_theme', 'citadelthemes_setup' );
+add_action( 'after_setup_theme', 'origin_setup' );
 
 /**
  * Register widget area.
  */
-function citadelthemes_widgets_init() {
+function origin_widgets_init() {
     register_sidebar( array(
-        'name'          => esc_html__( 'Sidebar', 'citadelthemes' ),
+        'name'          => esc_html__( 'Sidebar', 'origin' ),
         'id'            => 'sidebar-1',
-        'description'   => esc_html__( 'Add widgets here.', 'citadelthemes' ),
+        'description'   => esc_html__( 'Add widgets here.', 'origin' ),
         'before_widget' => '<div id="%1$s" class="widget %2$s">',
         'after_widget'  => '</div>',
         'before_title'  => '<h2 class="widget-title">',
         'after_title'   => '</h2>',
     ) );
 }
-add_action( 'widgets_init', 'citadelthemes_widgets_init' );
+add_action( 'widgets_init', 'origin_widgets_init' );
 
 /**
  *  Enqueue any scripts and styles required by the theme.
  */
-function citadelthemes_scripts() {
+function origin_scripts() {
 
     // Main stylesheet
-    wp_enqueue_style( 'citadelthemes-style', get_stylesheet_uri() );
+    wp_enqueue_style( 'origin-style', get_stylesheet_uri() );
 
     // Main script file
-    wp_enqueue_script( 'citadelthemes-scripts', get_template_directory_uri() . '/assets/js/all.js', array(), false, true );
+    wp_enqueue_script( 'origin-scripts', get_template_directory_uri() . '/assets/js/all.js', array(), false, true );
 
 }
-add_action( 'wp_enqueue_scripts', 'citadelthemes_scripts' );
+add_action( 'wp_enqueue_scripts', 'origin_scripts' );
 
 
 
